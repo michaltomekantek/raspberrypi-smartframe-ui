3552bc36a4a374576192eed27f4c74d71652c2fd
[dyad] <mixa460@gmail.com>
2026-02-03 19:13:54 +0100

    [dyad] Reset projektu do prostej strony Hello World - wrote 4 file(s), deleted 8 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-"use client";
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import UploadPage from './pages/UploadPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/upload" element={<UploadPage />} />
-      </Routes>
-    </Router>
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <h1 className="text-6xl font-bold tracking-tighter animate-pulse">
+        Hello World
+      </h1>
+    </div>
   );
 }
 
